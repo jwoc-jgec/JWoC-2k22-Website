ac8070bdde8dd96cb65070cbaa28937ec75f211c
imniladri <jnvniladri101@gmail.com>
2021-12-03 12:19:41 +0530

    Code Added

---
+++ js/data/event.data.js
@@ -0,0 +1,152 @@
+/* ********************************************************** */
+// Events Timeline Data Object
+/* ********************************************************** */
+
+const intDate = new Date().getTime();
+const intValue = 1000 * 60 * 60 * 24;
+
+let eventsTimeline = [
+    {
+        id: 1,
+        date: "25 December, 2021",
+        name: "JWoC Program Announced",
+        desc: "Here we go announced our JWoC program as a big start!",
+
+        // completeClass: "bx bx-check",
+        // completeStyle: "display:none",
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("25 December, 2021").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 2,
+        date: "25 December, 2021",
+        name: "Mentor & Mentee Registration",
+        desc: "All the mentors & mentees can start your registration from now on.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("25 December, 2021").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 3,
+        date: "15 January, 2022",
+        name: "Mentor Registration Ends",
+        desc: "Hope all the mentors have registered themselves & submitted their projects.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("15 January, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 4,
+        date: "18 January, 2022",
+        name: "Projects Announced",
+        desc: "Wait is over, We are going to reveal all the projects for this event.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("18 January, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 5,
+        date: "18 January, 2022",
+        name: "Community Bonding Starts",
+        desc: "Lets work together as good community for the next few weeks.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("18 January, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 6,
+        date: "20 January, 2022",
+        name: "Community Bonding Ends",
+        desc: "From now on we will work together!",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("20 January, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 7,
+        date: "21 January, 2022",
+        name: "Coding Period Starts",
+        desc: "Finally the moment is here, You can start working on your desired projects. Code Hard!",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("21 January, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 8,
+        date: "9 February, 2022",
+        name: "Phase I Completion",
+        desc: "Just our Phase I of the event is over!",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("9 February, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 9,
+        date: "19 February, 2022",
+        name: "Phase II Ends - JWoC Ends",
+        desc: "And at our last stage, JWoC comes to an end, its time to wait till next year.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("19 February, 2022").getTime() - intDate) / intValue
+        ),
+    },
+    {
+        id: 10,
+        date: "24 February, 2022",
+        name: "Result Announcement",
+        desc: "All participants get ready to know who is big winner of this year JWoC.",
+
+        completeClass: "",
+        completeStyle: "",
+        daysLeft: Math.floor(
+            (new Date("24 February, 2022").getTime() - intDate) / intValue
+        ),
+    },
+];
+
+/* ********************************************************** */
+// Events Preview Mapping
+/* ********************************************************** */
+
+let eventsDiv = document.querySelector(".events");
+
+Object.values(eventsTimeline).map((val) => {
+    eventsDiv.innerHTML += `
+        <!-- Event ${val.id} -->
+        <div class="event_card">
+            <i class="${val.completeClass}"></i>
+            <span style="${val.completeStyle}">
+                ${val.daysLeft} Days
+            </span>
+            <h2>${val.date}</h2>
+            <h4>${val.name}</h4>
+            <p>${val.desc}</p>
+        </div>
+    `;
+});
---
+++ js/data/team.data.js
@@ -0,0 +1,133 @@
+/* ********************************************************** */
+// Team Members Data Object
+/* ********************************************************** */
+
+let teamDetails = [
+    {
+        id: 1,
+        name: "Utpalendu Barman",
+        avatar: "../../assets/img/team/utpalendu_barman.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/utpalendubarman",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/utpalendubarman/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/badhanbarman",
+    },
+    {
+        id: 2,
+        name: "Niloy Sikdar",
+        avatar: "../../assets/img/team/niloy_sikdar.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/niloysikdar",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/niloysikdar/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/niloysikdar_",
+    },
+    {
+        id: 3,
+        name: "Anubhab Sarkar",
+        avatar: "../../assets/img/team/anubhab_sarkar.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/anubhab1710",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/anubhabsarkar/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/xperiencewhiz",
+    },
+    {
+        id: 4,
+        name: "Niladri Mondal",
+        avatar: "../../assets/img/team/niladri_mondal.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/imniladri",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/imniladrimondal/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/imniladrimondal",
+    },
+    {
+        id: 5,
+        name: "Shreya Paul",
+        avatar: "../../assets/img/team/shreya_paul.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/paulshreya",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/shreya-paul-05a0a61aa/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/__paul_shreya__",
+    },
+    {
+        id: 6,
+        name: "Aman Prakash Jha",
+        avatar: "../../assets/img/team/aman_prakash_jha.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/amanjha8100",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/aman-prakash-jha/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/aman_ItIs",
+    },
+    {
+        id: 7,
+        name: "Sagnik Roy",
+        avatar: "../../assets/img/team/sagnik_roy.jpg",
+        desc: "Organizer",
+
+        socialIcon1: "bx bxl-github",
+        socialLink1: "https://github.com/sagnik1511",
+
+        socialIcon2: "bx bxl-linkedin",
+        socialLink2: "https://www.linkedin.com/in/sagnik1511/",
+
+        socialIcon3: "bx bxl-twitter",
+        socialLink3: "https://twitter.com/Agnik1511S",
+    },
+];
+
+/* ********************************************************** */
+// Team Members Preview Mapping
+/* ********************************************************** */
+
+let teamDiv = document.querySelector(".members");
+
+Object.values(teamDetails).map((val) => {
+    teamDiv.innerHTML += `
+        <!-- Member ${val.id} -->
+        <div class="member_card">
+            <img src="${val.avatar}" alt="Profile Img" />
+            <h2>${val.name}</h2>
+            <p><span>${val.desc}</span></p>
+            <div class="social">
+                <a href="${val.socialLink1}" target="_blank"><i class="${val.socialIcon1}"></i></a>
+                <a href="${val.socialLink2}" target="_blank"><i class="${val.socialIcon2}"></i></a>
+                <a href="${val.socialLink3}" target="_blank"><i class="${val.socialIcon3}"></i></a>
+            </div>
+        </div>
+    `;
+});
