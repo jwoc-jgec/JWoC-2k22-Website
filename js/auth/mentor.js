9b266ec13067db69495a84a9c4974fb3f60d87f8
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-12 19:02:13 +0530

    fix: Endpoint fix

--- js/auth/mentor.js
+++ js/auth/mentor.js
@@ -51,7 +51,7 @@ authForm.addEventListener("submit", (e) => {
 });
 
 const submitForm = (data) => {
-  fetch("http://localhost:5000/mentor", {
+  fetch("https://jwoc-backend.glitch.me/mentor", {
     method: "post",
     headers: {
       Accept: "application/json, text/plain, */*",
